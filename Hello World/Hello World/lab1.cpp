5278190198b91d20b28043a03bd0cf3333745ac6
daniel <dzhangg0614@gmail.com>
2018-01-15 15:00:52 -0800

    Lab1 is done

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -1,6 +1,5 @@
 #include "lab1.h"
 
-
 // Greatest Common Denominator
 // PRE: a is a positive integer
 // PRE: b is a positive integer
@@ -8,8 +7,18 @@
 // RETURN: the greatest common denominator of a and b.
 int gcd(const int a, const int b)
 {
-	// Replace this line with your code (have fun!).
-	return 0;
+	if (a == 0 || b == 0)
+		return 0;
+
+	// base case
+	if (a == b)
+		return a;
+
+	// a is greater
+	if (a > b) {
+		return gcd(a - b, b);
+	}
+	return gcd(a, b - a);
 }
 
 // Fibonacci
@@ -18,6 +27,11 @@ int gcd(const int a, const int b)
 // RETURN: the nth positive integer in the Fibonacci sequence.
 long long fibonacci(const int n)
 {
-	// Replace this line with your code (challenge: do this with recursion!).
-	return 0;
+	if (n == 0 || n == 1) {
+		return n;
+	}
+
+	else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
 }
